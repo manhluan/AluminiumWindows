75718eb83a02b715cc54ee96fcb3d67712ef3ef9
datnd <nguyenducdatdeveloper@gmail>
2022-03-14 21:17:13 +0700

    init: initial base for web

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.scss";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="#" element={#} />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
