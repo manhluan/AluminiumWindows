b8ba98f4fc2f7973bf6b34ce9123055f76b365f0
luctran23 <anhvanluc1999@gmail.com>
2022-03-17 07:02:58 +0700

    feature/home/bathroom-section implementation

--- src/router/homepage/Home.component.tsx
+++ src/router/homepage/Home.component.tsx
@@ -2,11 +2,13 @@ import "./Home.style.scss";
 import Banner from "components/banner";
 import Layout from "components/layout/Layout.component";
 import Footer from "components/footer";
+import Bathroom from "components/bathroom/Bathroom.component";
 const Home = () => {
     return (
         <Layout>
             <div className="homepage">
                 <Banner />
+                <Bathroom />
                 <Footer></Footer>
             </div>
         </Layout>
